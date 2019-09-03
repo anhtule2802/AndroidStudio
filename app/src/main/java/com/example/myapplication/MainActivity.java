39d350ef76a94f2620b32a666c507e3ca692f21e
anhtule2802 <anhtule2802@gmail.com>
2019-09-03 16:23:39 +0700

    4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Alert Dialog
     TextView tv_ketqua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +18,21 @@ public class MainActivity extends AppCompatActivity {
         mydialog.setTitle("Message");
         tv_ketqua = (TextView)findViewById(R.id.tv_ketqua);
         final CharSequence[] items = {"Đỏ","Vàng","Cam"};
-        mydialog.setItems(items, new DialogInterface.OnClickListener() {
+        final boolean[] arrayCheck = {false,false,false};
+        mydialog.setMultiChoiceItems(items, arrayCheck, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                arrayCheck[i] = b;
+            }
+        });
+        mydialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                tv_ketqua.setText(items[i].toString());
+                String st="";
+                for(int j=0;j<items.length;j++)
+                    if(arrayCheck[j])
+                        st += items[j].toString()+"\n";
+                    tv_ketqua.setText(st);
             }
         });
         AlertDialog alertDialog = mydialog.create();
