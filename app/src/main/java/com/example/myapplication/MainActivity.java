ba7dd47bf8acb82d058a0f18a78e4fe9fcf75aa4
anhtule2802 <anhtule2802@gmail.com>
2019-09-03 15:52:39 +0700

    3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AlertDialog.Builder mydialog = new AlertDialog.Builder(this);
+        mydialog.setTitle("Message");
+        tv_ketqua = (TextView)findViewById(R.id.tv_ketqua);
+        final CharSequence[] items = {"Đỏ","Vàng","Cam"};
+        mydialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tv_ketqua.setText(items[i].toString());
+            }
+        });
+        AlertDialog alertDialog = mydialog.create();
+        alertDialog.show();
     }
 }
